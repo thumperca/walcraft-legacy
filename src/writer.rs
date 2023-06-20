367c2e77a76fbef0e501333af6848dae405e9dcf
Manvir Singh <mail@manvir.me>
2023-06-20 13:23:38 +0530

    Create files on storage

--- src/writer.rs
+++ src/writer.rs
@@ -1,5 +1,6 @@
 use crate::WalError;
-use std::fs::File;
+use std::fmt::format;
+use std::fs::{File, OpenOptions};
 use std::io::Write;
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
@@ -7,6 +8,8 @@ use std::sync::{Arc, Mutex};
 pub(crate) struct WalWriter {
     // shared buffer
     buffer: Arc<Mutex<Vec<Vec<u8>>>>,
+    // Location where files are stored
+    location: PathBuf,
     // Handle to current file
     file: File,
     // storage capacity per file
@@ -19,33 +22,57 @@ pub(crate) struct WalWriter {
 
 impl WalWriter {
     pub fn new(location: &str, capacity: usize) -> Result<Self, WalError> {
-        // let base_path = PathBuf::from(location);
-        // let mut pointer_path = base_path.clone();
-        // pointer_path.push("pointer");
-        // if pointer_path.exists() {
-        //     // load existing files
-        // } else {
-        //     // create a new file for writing logs
-        //     let mut file = match File::create(file_path) {
-        //         Ok(f) => f,
-        //         Err(_) => {
-        //             return Err(WalError::File("Failed to create pointer file".to_string()));
-        //         }
-        //     };
-        //     // write current pointer
-        //     if let Err(_) = file.write_all(content.as_bytes()) {
-        //         return Err(WalError::File(
-        //             "Failed to write to pointer file".to_string(),
-        //         ));
-        //     }
-        //     // create the 5 buffer files
-        //     for i in 1..6 {
-        //         let file_name = format!("wal_{}", i);
-        //         let mut path = base_path.clone();
-        //         path.push(file_name);
-        //     }
-        // }
+        let base_path = PathBuf::from(location);
 
-        todo!()
+        let buffer = Arc::new(Mutex::new(Vec::new()));
+
+        let pointer = 1u8;
+        let (file, filled) = Self::set_pointer(base_path.clone(), pointer)?;
+
+        let wal = Self {
+            buffer,
+            location: base_path,
+            file,
+            capacity_per_file: capacity / 5,
+            filled,
+            pointer,
+        };
+        Ok(wal)
+    }
+
+    fn set_pointer(location: PathBuf, pointer: u8) -> Result<(File, usize), WalError> {
+        // write pointer to meta file
+        Self::write_pointer(location.clone(), pointer)?;
+        // open and return pointer WAL file
+        Self::open_file(location, pointer).map(|file| (file, 0))
+    }
+
+    fn write_pointer(mut location: PathBuf, pointer: u8) -> Result<(), WalError> {
+        location.push("meta");
+        // create a new file for writing logs
+        let mut file = match File::create(location) {
+            Ok(f) => f,
+            Err(_) => {
+                return Err(WalError::File("Failed to create pointer file".to_string()));
+            }
+        };
+        // write current pointer
+        let text = pointer.to_string();
+        if let Err(_) = file.write_all(text.as_bytes()) {
+            return Err(WalError::File(
+                "Failed to write to pointer file".to_string(),
+            ));
+        }
+        Ok(())
+    }
+
+    fn open_file(mut location: PathBuf, pointer: u8) -> Result<File, WalError> {
+        let file_name = format!("wal_{}", pointer);
+        location.push(file_name);
+        OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(&location)
+            .map_err(|_| WalError::File("Failed to open log file".to_string()))
     }
 }
